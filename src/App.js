d1f414a853fa6d184e62d7b6ea4b7b798ccb3748
Reynolds Sharp <sharpdesignco@Reynoldss-iPro.attlocal.net>
2022-07-20 13:25:38 -0500

    router basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from 'react-router-dom';
 
-function App() {
+export default function App() {
+  const posts = [
+    {
+      id: 1,
+      title: 'my first post',
+      date: '4-7-22',
+      content: 'This is my first post!'
+    },
+    {
+      id: 2,
+      title: 'checking in',
+      date: '4-8-22',
+      content: 'wow this is good food!'
+    },
+    {
+      id: 3,
+      title: 'new car',
+      date: '4-9-22',
+      content: 'Look what bb bought me!'
+    }
+  ];
+
+  return(
+    <Router> 
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/friends">Friends</Link>
+            </li>
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/posts">
+              <Posts posts={posts} />
+            </Route>
+            <Route path="/friends">
+              <Friends names={['Tom', 'Sara', 'Henry', 'Bobert', 'Clyde']} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Friends(props) {
+  const { names } = props; 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Friends</h2>
+      <ul>
+        {names.map((friend, index) => (
+          <li key={index}>{friend}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function Posts({ posts }) {
+  const match = useRouteMatch();
+  const findPostById = (id) => 
+    posts.filter((post) => post.id == id)[0];
+  return (
+  <div>
+    <h2>Posts</h2>
+    <ul>
+      {posts.map((post, index) => {
+        return (
+          <li key={index}>
+            <Link to={`${match.url}/${post.id}`}>
+              {post.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+    <Switch>
+      <Route
+        path={`${match.path}/:postId`}
+        render={(props) => (
+          <Post
+            {...props}
+            data={findPostById(props.match.params.postId)}
+          />
+        )}
+      />
+      <Route path={match.path}>
+        <h3>Please select a post.</h3>
+      </Route>
+    </Switch>
+  </div>
+  )
+}
+
+function Post(props) {
+  const { data } = props;
+  return (
+    <div>
+      <h3>{data.title}</h3>
+      <h4>{data.date}</h4>
+      <p>{data.content}</p>
+    </div>
+  );
+}
